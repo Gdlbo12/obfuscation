2c435b8bc0ec49502dc09c946260db17da2a8884
unexsid <62994434+Gdlbo12@users.noreply.github.com>
2025-06-18 11:31:14 +0800

    add generateRandomName

--- PRAKTIKA.cpp
+++ PRAKTIKA.cpp
@@ -9,8 +9,17 @@
 #include <filesystem>
 
 // Функция для генерации случайных имен переменных
-std::string generateRandomName() {
-
+std::string generateRandomName(int length) {
+    static const std::string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> dis(0, chars.size() - 1);
+    
+    std::string result;
+    for (int i = 0; i < length; ++i) {
+        result += chars[dis(gen)];
+    }
+    return result;
 }
 
 // Функция для шифрования строк с использованием XOR
